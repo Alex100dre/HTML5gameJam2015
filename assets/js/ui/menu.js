e713bd38be987cee46377062674f60feebc5beab
Alex100dre <alex100dre@gmail.com>
2015-04-12 01:17:54 +0200

    Menu & btn responsive

--- assets/js/ui/menu.js
+++ assets/js/ui/menu.js
@@ -1,19 +1,31 @@
 app.ui.menu = {
 	preload: function(){
         // Images
-        // game.load.image('bg', 'assets/images/sprites/ui/menu/bg.jpg');
-        // game.load.spritesheet('btn2', 'assets/images/sprites/ui/menu/play.png', 345, 183);
-        // game.load.spritesheet('btn1', 'assets/images/sprites/ui/menu/options.png', 345, 183);
-        game.load.spritesheet('btn0', 'assets/img/ui/menu/jouer.png', 256, 100);
+        game.load.image('bg', 'assets/img/ui/menu/bg.jpg');
+        game.load.spritesheet('btn0', 'assets/img/ui/menu/jouer.png', 392, 203);
         // GLOBAL SOUNDS
         // game.load.audio('themeMenu',  ['assets/audio/theme/menu/theme.mp3', 'assets/audio/theme/menu/theme.ogg']); 
 
     },
 
     create: function(){
+        bg=game.add.sprite(0,0,'bg');
+
+        var bHeight = game.camera.height;
+
+        var bWidth = (bg.width * bHeight)/bg.height;
+
+        bg.width = bWidth;
+        bg.height = bHeight;
+        bg.x = game.canvas.width*.5-bg.width*.5;
+
+
     	button = game.add.button(game.world.centerX - 256, game.world.centerY-100, 'btn0', app.ui.menu.actionOnClick, this, 2, 1, 0);
     	button.name = 'play';
     	button.input.useHandCursor = true;
+        button.alpha = 0;
+
+        game.add.tween(button).to( { alpha: 1 }, 1000, Phaser.Easing.Bounce.Out, true);
     },
 
     gofull : function() {
